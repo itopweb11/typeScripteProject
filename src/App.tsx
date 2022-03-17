403d52fd02e55e0188a3ec7fea813dc5e5d0439b
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2022-03-17 20:36:46 +0300

    TypeScript Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Navbar} from "./components/Navbar";
 import {TodoForm} from "./components/TodoForm";
 import {TodoList} from "./components/TodoList";
 import {ITodo} from "./interfaces";
 
+declare var confirm: (question: string) => boolean
+
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addHandler = (title: string) => {
     const newTodo: ITodo = {
@@ -42,7 +53,10 @@ const App: React.FC = () => {
   }
 
   const removeHandler = (id: number) => {
-    setTodos(prev => prev.filter(todo => todo.id !== id))
+    const shoudRemove = confirm('Вы уверены б что хотите удалить элемент?')
+    if (shoudRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
   }
 
   return <>
